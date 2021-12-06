5129e90b1e42638cce1fe4c9e59cac37dc28d676
Anders Jändel <sviams@gmail.com>
2021-12-06 13:29:12 +0100

    Day 5 cleanup

--- src/main/kotlin/day5.kt
+++ src/main/kotlin/day5.kt
@@ -7,11 +7,11 @@ object day5 {
     data class Vector(val start: Pos, val end: Pos) {
         val nodes: List<String> by lazy {
             val steps = max(max(start.x, end.x) - min(start.x, end.x), max(start.y, end.y) - min(start.y, end.y))
-            val xSteps = if (start.x == end.x) (0 .. steps).map { start.x } else if (start.x > end.x) (start.x downTo end.x).toList() else (start.x .. end.x).toList()
-            val ySteps = if (start.y == end.y) (0 .. steps).map { start.y } else if (start.y > end.y) (start.y downTo end.y).toList() else (start.y .. end.y).toList()
+            val xSteps = if (start.x == end.x) (0 .. steps).map { start.x } else IntProgression.fromClosedRange(start.x, end.x, if (start.x < end.x) 1 else -1)
+            val ySteps = if (start.y == end.y) (0 .. steps).map { start.y } else IntProgression.fromClosedRange(start.y, end.y, if (start.y < end.y) 1 else -1)
             xSteps.zip(ySteps).map { "${it.first},${it.second}" }
         }
-        fun isVertical() = (start.x == end.x || start.y == end.y)
+        fun isStraight() = (start.x == end.x || start.y == end.y)
     }
 
     fun parseLines(input: List<String>): List<Vector> = input.map { line ->
@@ -19,15 +19,12 @@ object day5 {
         Vector(Pos(x1.toInt(), y1.toInt()), Pos(x2.toInt(), y2.toInt()))
     }
 
-    fun pt1(input: List<String>): Int =
-        parseLines(input).filter { it.isVertical() }.flatMap { it.nodes }.fold(persistentHashMapOf<String, Int>()) { freqMap, node ->
-            val existingValue = freqMap.getOrDefault(node, 0)
-            freqMap.put(node, existingValue+1)
-        }.values.count { it > 1 }
+    fun List<Vector>.countIntersections(): Int = flatMap { it.nodes }
+        .fold(persistentHashMapOf<String, Int>()) { freqMap, node -> freqMap.put(node, freqMap.getOrDefault(node, 0)+1) }
+        .values.count { it > 1 }
 
-    fun pt2(input: List<String>): Int =
-        parseLines(input).flatMap { it.nodes }.fold(persistentHashMapOf<String, Int>()) { freqMap, node ->
-            val existingValue = freqMap.getOrDefault(node, 0)
-            freqMap.put(node, existingValue+1)
-        }.values.count { it > 1 }
+    fun pt1(input: List<String>): Int = parseLines(input).filter { it.isStraight() }.countIntersections()
+
+    fun pt2(input: List<String>): Int = parseLines(input).countIntersections()
+
 }
