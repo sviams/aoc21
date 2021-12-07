a2027827a0ab675ffce8f09aa6c14a37b46f25c2
Anders Jändel <sviams@gmail.com>
2021-12-07 15:35:22 +0100

    Day 7 cleanup

--- src/main/kotlin/day7.kt
+++ src/main/kotlin/day7.kt
@@ -4,22 +4,17 @@ object day7 {
 
     fun parseInput(input: List<String>): List<Int> = input.first().split(",").map { it.toInt() }
 
-    tailrec fun findLowestFuelCost(positions: List<Int>, current: Int = positions.minOrNull()!!, max: Int = positions.maxOrNull()!!, result: Long = Long.MAX_VALUE, accFunc: (Long, Int, Int) -> Long): Long {
+    tailrec fun findLowestFuelCost(positions: List<Int>, current: Int = positions.minOrNull()!!, max: Int = positions.maxOrNull()!!, result: Long = Long.MAX_VALUE, accFunc: (Int, Int) -> Long): Long {
         if (current == max) return result
-        val cost = positions.fold(0L) { acc, pos -> accFunc(acc, current, pos) }
+        val cost = positions.fold(0L) { acc, pos -> acc + accFunc(current, pos) }
         return findLowestFuelCost(positions, current+1, max, if (cost < result) cost else result, accFunc)
     }
 
-    fun pt1(input: List<String>): Long =
-        findLowestFuelCost(parseInput(input)) { totalCostAtCurrent, current, pos ->
-            totalCostAtCurrent + abs(current - pos)
-        }
+    fun pt1(input: List<String>): Long = findLowestFuelCost(parseInput(input)) { current, pos -> abs(current - pos).toLong() }
 
-    fun pt2(input: List<String>): Long =
-        findLowestFuelCost(parseInput(input)) { totalCostAtCurrent, current, pos ->
-            totalCostAtCurrent + (1 .. abs(current - pos)).fold(0L) { costForPosition, distanceFromCurrent ->
-                costForPosition + distanceFromCurrent
-            }
+    fun pt2(input: List<String>): Long = findLowestFuelCost(parseInput(input)) { current, pos ->
+            val n = abs(current - pos)
+            n*(n+1)/2L // == 1+2+..+n
         }
 
 }
